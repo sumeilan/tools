78447518981e077a1c6c6f271b4a7c663d90fdf3
sumeilan <912041511@qq.com>
2019-09-08 20:00:49 +0800

    9.8

--- process_data.py
+++ process_data.py
@@ -7,16 +7,16 @@ def clear_blank_line():
 	new2 = []
 
 	exceldatas = excel_to_json.excel_to_json('E:\\test_python\\tools\\datas\\test3.xlsx')
-	print(exceldatas)
+	# print(exceldatas)
 	for i in range(len(exceldatas)):
 
 		for y in range(len(exceldatas[i])):
 			# new = re.sub(r'[\n]+', r'\n', exceldatas[i][y], flags=re.S)#去除多余空格
-			new = re.sub(r' ', r'\n', exceldatas[i][y], flags=re.S)#空格替换为换行符
+			new = re.sub(r'[ ]+', r'\n', exceldatas[i][y], flags=re.S)#空格替换为换行符
 			new1.append(new)
 		new2.append(new1)
 		new1 = []
-	print(new2)
+	# print(new2)
 	excel_to_json.json_to_excel_new(new2,'test4.xlsx')
 
 if __name__ == '__main__':
