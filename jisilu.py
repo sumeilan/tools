d31d9895f5919dddcb1db9bc1d42fb398c8478e0
sumeilan <912041511@qq.com>
2020-02-28 14:06:44 +0800

    11.13

--- jisilu.py
+++ jisilu.py
@@ -15,35 +15,37 @@ class fund:
                     fund.append(d_QDII)
         return fund
 
-    def get_QDII(self):
+    def get_QDII(self,fund_ids):
         fund = [('QDII基金代码', '基金名称', '现价', '净值', '最新估值', '溢价率')]
         path  = 'qdii/qdii_list/?___jsl=LST___t= '
         url = self.host + path + str(self.mstime)
         response = requests.get(url)
+        # print(response.text)
         datas = json.loads(response.text)
-        fund_ids = [162411, 159920, 513520]
         QDII_fund = self.get_fund_id(datas['rows'], fund_ids)
         return fund + QDII_fund
 
-    def get_ETF(self):
+    def get_ETF(self,fund_ids):
         fund = [('ETF基金代码', '基金名称', '现价', '净值', '最新估值', '溢价率')]
         path = 'etf/etf_list/?___jsl=LST___t='
         url = self.host + path + str(self.mstime)
         response = requests.get(url)
         datas = json.loads(response.text)
-        fund_ids = [512580, 512980, 512880]
         QDII_fund = self.get_fund_id(datas['rows'], fund_ids)
         return fund + QDII_fund
 
+    def show_fund(self,*fund):
+        for l in fund:
+            for k in l:
+                print(k)
+            print('\n')
 
 F = fund()
-QDII_fund = F.get_QDII()
-for i in QDII_fund:
-    print(i)
-print('\n')
-ETF_fund = F.get_ETF()
-for i in ETF_fund:
-    print(i)
+qdii_fund_ids =[162411, 159920, 513520,160416]
+etf_fund_ids =  [512580, 512980, 512880,515180,510300,510510,159938]
+QDII_fund = F.get_QDII(qdii_fund_ids)
+ETF_fund = F.get_ETF(etf_fund_ids)
+F.show_fund(QDII_fund,ETF_fund)
 
 
 
